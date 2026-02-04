cad6a91414f06df877dcfd6758be3debbc70b1eb
Taehoon Kim <taehoonkim.dev@gmail.com>
2026-02-04 17:21:25 +0900

    chore: 환경변수명 변경

--- app/core/database.py
+++ app/core/database.py
@@ -2,9 +2,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from app.core.config import settings
 
-SQLALCHEMY_DATABASE_URL = settings.DATABASE_URL
+SQLALCHEMY_DB_URL = settings.DB_URL
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(SQLALCHEMY_DB_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
