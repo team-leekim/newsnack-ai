e1f9700f666229e6469fbdfd3110823b957f63a8
Taehoon Kim <taehoonkim.dev@gmail.com>
2026-02-04 21:14:48 +0900

    chore: 불필요한 변수 제거

--- app/core/database.py
+++ app/core/database.py
@@ -2,9 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from app.core.config import settings
 
-SQLALCHEMY_DB_URL = settings.DB_URL
-
-engine = create_engine(SQLALCHEMY_DB_URL)
+engine = create_engine(settings.DB_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
